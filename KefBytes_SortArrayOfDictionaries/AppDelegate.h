414fa20eb44201d9e53ef7873b7a032a3821da87
Kent Franks <Kent.Franks@tiaa.org>
2016-10-18 09:57:52 -0400

    Initial Commit

---
+++ KefBytes_SortArrayOfDictionaries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KefBytes_SortArrayOfDictionaries
+//
+//  Created by Franks, Kent Eric on 10/18/16.
+//  Copyright © 2016 KefBytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KefBytes_SortArrayOfDictionaries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KefBytes_SortArrayOfDictionaries
+//
+//  Created by Franks, Kent Eric on 10/18/16.
+//  Copyright © 2016 KefBytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
