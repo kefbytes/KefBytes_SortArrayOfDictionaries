4b0e186844cc875f27536ce23e7a3fa7b401dc4f
Kent Franks <kent@kefbytes.com>
2016-10-26 11:08:39 -0400

    first commit

--- KefBytes_SortArrayOfDictionaries/ViewController.m
+++ KefBytes_SortArrayOfDictionaries/ViewController.m
@@ -10,18 +10,65 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray *transactionsArray;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self fetchJSONResourceWithName:(@"TransactionsArray") completionHandler:^(NSDictionary *json, NSError *error) {
+        self.transactionsArray = json[@"PendingTransaction"];
+        NSLog(@"transactionsArray = %@", self.transactionsArray);
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)fetchJSONResourceWithName:(NSString *)name completionHandler:(void(^)(NSDictionary *json, NSError *error))handler
+{
+    NSParameterAssert(handler != nil);
+    
+    NSURL *resourceURL = [[NSBundle mainBundle] URLForResource:name withExtension:@"json"];
+    if (!resourceURL) {
+        // Should create an NSError and pass it to the completion handler
+        NSAssert(NO, @"Could not find resource: %@", name);
+    }
+    
+    NSError *error;
+    
+    // Fetch the json data. If there's an error, call the handler and return.
+    NSData *jsonData = [NSData dataWithContentsOfURL:resourceURL options:NSDataReadingMappedIfSafe error:&error];
+    if (!jsonData) {
+        handler(nil, error);
+        return;
+    }
+    
+    // Parse the json data. If there's an error parsing the json data, call the handler and return.
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
+    if (!json) {
+        handler(nil, error);
+        return;
+    }
+    
+    // If the json data specified that we should delay the results, do so before calling the handler
+    NSNumber *delayResults = json[@"delayResults"];
+    if (delayResults && [delayResults isKindOfClass:[NSNumber class]]) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)([delayResults floatValue] * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            handler(json, nil);
+        });
+    }
+    else {
+        handler(json, nil);
+    }
 }
+
+- (IBAction)sortArray:(id)sender {
+    NSSortDescriptor *sequenceDescriptor = [[NSSortDescriptor alloc]initWithKey:@"Sequence"  ascending:NO selector:@selector(localizedStandardCompare:)];
+    NSArray *sortDescriptors = @[sequenceDescriptor];
+    NSArray *sortedArray = [self.transactionsArray sortedArrayUsingDescriptors:sortDescriptors];
+    NSLog(@"sortedArray = %@", sortedArray);
+}
+
 
 @end
